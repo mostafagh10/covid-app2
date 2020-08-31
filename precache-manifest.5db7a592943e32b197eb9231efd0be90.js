49b881781d5458408bc8c0404beebc3569ee7d46
mostafa ghazaly <ghazalymostafa07@gmail.com>
2020-08-31 06:17:15 +0200

    Updates

---
+++ precache-manifest.5db7a592943e32b197eb9231efd0be90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c28bbc07d1b77c4f57cb1d5a13dcc9a",
+    "url": "/covid-app2/index.html"
+  },
+  {
+    "revision": "d96b7aca535afb11b68b",
+    "url": "/covid-app2/static/css/main.ccf19aae.chunk.css"
+  },
+  {
+    "revision": "0213866d5d7cfc6d6f4a",
+    "url": "/covid-app2/static/js/2.0671bdd0.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/covid-app2/static/js/2.0671bdd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d96b7aca535afb11b68b",
+    "url": "/covid-app2/static/js/main.560708cf.chunk.js"
+  },
+  {
+    "revision": "663ffa648728f5bb87f3",
+    "url": "/covid-app2/static/js/runtime-main.b613f0fa.js"
+  }
+]);
